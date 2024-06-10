e0442e0df0a53c43f8ae88e45a7c1a14947ad344
immails <anton140315@gmail.com>
2024-06-10 20:51:06 +0700

    Gradle + ForgeGradle + Forge + Gitigore Update

--- src/main/java/net/blay09/mods/hardcorerevival/HardcoreRevivalManager.java
+++ src/main/java/net/blay09/mods/hardcorerevival/HardcoreRevivalManager.java
@@ -79,7 +79,7 @@ public class HardcoreRevivalManager implements IHardcoreRevivalManager {
                         server.getPlayerList().sendMessageToTeamOrAllPlayers(player, player.getCombatTracker().getDeathMessage());
                     }
                 } else {
-                    server.getPlayerList().func_232641_a_(player.getCombatTracker().getDeathMessage(), ChatType.SYSTEM, Util.DUMMY_UUID); // sendMessage
+                    server.getPlayerList().func_232641_a_(source.getDeathMessage(player), ChatType.SYSTEM, Util.DUMMY_UUID); // sendMessage
                 }
             }
         }
